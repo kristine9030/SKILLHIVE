e6caf5f50c3449e9548f09e827b9ab84f9639807
johndavebriones <johndavealdaybriones009@gmail.com>
2026-04-26 08:43:22 +0800

    added vendor for php smtp

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dabideyy/skillhive',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '916bf1f6ab008f8b3bc8644ce35bd1c7a180ceb5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dabideyy/skillhive' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '916bf1f6ab008f8b3bc8644ce35bd1c7a180ceb5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v7.0.2',
+            'version' => '7.0.2.0',
+            'reference' => 'ebf1655bd5b99b3f97e1a3ec0a69e5f4cd7ea088',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
